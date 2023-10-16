153281d6142c02e977c8f4337e6eed209409f846
MuhammadAhmed-Developer <ahmadginideveloper@gmail.com>
2023-10-17 01:01:03 +0500

    first commit

---
+++ Class01/class1.py
@@ -0,0 +1,6 @@
+print("Hellow World")
+name : str = "Pakistan"
+print(name) # print
+print(type(name)) # type
+print(id(name)) # physcial address
+print([i for i in dir(name) if "__" not in i]) # methods and attributes
